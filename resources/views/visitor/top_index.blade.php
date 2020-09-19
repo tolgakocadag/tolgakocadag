f29d08964f2baf1e2606f45c02313293f5ae79c0
tolgakocadag <tolgakocadag@outlook.com>
2020-09-19 22:47:35 +0300

    analytics google eklendi

--- resources/views/visitor/top_index.blade.php
+++ resources/views/visitor/top_index.blade.php
@@ -36,6 +36,8 @@
       gtag('config', 'UA-178422037-1');
     </script>
 
+    <script data-ad-client="ca-pub-4320204669065193" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
 </head>
 <body onload="startTime()" class="visitor_scrollbar">
 <!--Load Page-->
